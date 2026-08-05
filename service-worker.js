f513d304c0a04a2340da0394b7f436a6ca73f494
ahmadok12 <ahmad.ok12@gmail.com>
2026-08-05 19:16:50 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scs-erp-phase2';
+const CACHE_NAME = 'scs-erp-phase2-1';
 const APP_SHELL = ['./index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
